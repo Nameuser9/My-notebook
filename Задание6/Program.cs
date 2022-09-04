276c456d1cfc1fc64afd8177e9fbead1a6bff93f
Nameuser9 <ivamand2002@gmail.com>
2022-09-04 12:40:50 +0700

    Приветствие ТОЛЬКО маши

---
+++ Задание6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите своё имя");
+string username = Console.ReadLine();
+if (username.ToLower() =="маша")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else 
+{
+    Console.Write("Привет,");
+    Console.Write(username);
+}
