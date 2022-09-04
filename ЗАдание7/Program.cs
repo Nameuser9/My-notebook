870d7461000adbda542c3d2343f2bc9825ef0b06
Nameuser9 <ivamand2002@gmail.com>
2022-09-04 14:34:24 +0700

    самое большое число

---
+++ ЗАдание7/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 3;
+int b = 4;
+int c = 1;
+int d = 2;
+int e = 0;
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+Console.Write("Самое большое число ");
+Console.Write( max);
